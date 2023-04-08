43481704d541da06730769068afbfbf90c6caeed
Mohammed Shaikh <shaikh.adnan@hotmail.com>
2023-04-08 09:48:57 -0400

    Finished Pseudo Code for hamburger icon on responsive design

---
+++ script.js
@@ -0,0 +1,16 @@
+//PSUEDO CODE 
+
+//Things to keep in mind when you work on this project
+    // make sure both index.html and contact.html have the script loaded/connected.
+    // start with what you know and look at notes accordingly.
+
+// First, we need to go get the hamburger icon from the page (using .querySelector).
+// Get a second item from the page being the UL of the nav to manipulate with JS.
+
+// Then, we attach an event listener to that icon that listens for the click event.
+
+// When the user clicks on the hamburger icon:
+    // Make sure that the hamburger icon is always on top (z index in css)
+    // toggle the ul to pop out and in on command/click
+    // create a function that handles the active state for both constants (hamburger icon and ul)
+    // Find out what the user clicked on our navigation and go to the loaded page.
